a5a7d58b6144f9365b91a4b393b3fc07cf29eb19
Pmookerjee <pmookerjee@gmail.com>
2017-04-29 16:16:39 -0700

    did all the things

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,7 +4,10 @@
   const aboutController = {};
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+ aboutController.showAbout = () => {
+   $('#articles').hide();
+   $('#about').show();
+ }
 
   module.aboutController = aboutController;
 })(window);
