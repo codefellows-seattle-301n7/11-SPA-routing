4d87c731009720ef699e404e184db51b9f99e926
sgordon834 <sgordon834@gmail.com>
2017-04-29 16:44:40 -0700

    Completed first 3 TODOs

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -4,7 +4,13 @@
   const articleController = {};
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
+
   // Also be sure to hide all the main section elements, and reveal the #articles section:
+  articleController.index = () => {
+    $('#articles').hide();
+    $('#articles').fadeIn('slow');
+  };
+
 
 
   module.articleController = articleController;
